bf13abb551f708aab8314dd78db73bb81d48c752
gdelgado <gdelgado2@gmail.com>
2011-03-01 08:14:59 -0500

    Fraction Calculator iPhone

---
+++ Fraction_Calculator/DerivedData/Fraction_Calculator/Build/Products/Debug-iphonesimulator/Fraction_CalculatorTests.octest/Fraction_CalculatorTests.h
@@ -0,0 +1,17 @@
+//
+//  Fraction_CalculatorTests.h
+//  Fraction_CalculatorTests
+//
+//  Created by Gerardo Delgado  on 2/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Fraction_CalculatorTests : SenTestCase {
+@private
+    
+}
+
+@end
