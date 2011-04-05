a8860830a107384c597fb4a7eb2dffb2cef6934c
gdelgado <gdelgado2@gmail.com>
2011-04-05 00:53:34 -0400

    Chapter 9 - Nav - iPhone Dev Book

--- iPhone4DevBook/Nav/Nav/DeleteMeController.h
+++ iPhone4DevBook/Nav/Nav/DeleteMeController.h
@@ -6,11 +6,14 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "SecondLevelViewController.h"
 
-
-@interface DeleteMeController : NSObject {
+@interface DeleteMeController : SecondLevelViewController {
+    NSMutableArray *list;
     
 }
+@property (nonatomic,retain) NSMutableArray *list;
+-(IBAction)toggleEdit:(id)sender;
 
 @end
--- iPhone4DevBook/Nav/Nav/DeleteMeController.m
+++ iPhone4DevBook/Nav/Nav/DeleteMeController.m
@@ -10,5 +10,73 @@
 
 
 @implementation DeleteMeController
+@synthesize list;
+
+-(IBAction)toggleEdit:(id)sender {
+    [self.tableView setEditing:!self.tableView.editing animated:YES];
+    
+    if (self.tableView.editing)
+        [self.navigationItem.rightBarButtonItem setTitle:@"Done"];
+    else
+        [self.navigationItem.rightBarButtonItem setTitle:@"Delete"];
+}
+
+-(void)viewDidLoad {
+    if (list == nil)
+    {
+        NSString *path = [[NSBundle mainBundle]
+                          pathForResource:@"computers" ofType:@"plist"];
+        NSMutableArray *array = [[NSMutableArray alloc]
+                                 initWithContentsOfFile:path];
+        self.list = array;
+        [array release];
+    }
+    UIBarButtonItem *editButton = [[UIBarButtonItem alloc]
+                                   initWithTitle:@"Delete"
+                                   style:UIBarButtonItemStyleBordered
+                                   target:self
+                                   action:@selector(toggleEdit:)];
+    self.navigationItem.rightBarButtonItem = editButton;
+    [editButton release];
+    
+    [super viewDidLoad];
+    
+}
+
+-(void)dealloc {
+    [list release];
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Table Data Source Methods
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [list count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *DeleteMeCellIdentifier = @"DeleteMeCellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:DeleteMeCellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc]
+                 initWithStyle:UITableViewCellStyleDefault reuseIdentifier:DeleteMeCellIdentifier]autorelease];
+        
+    }
+    NSInteger row = [indexPath row];
+    cell.textLabel.text = [self.list objectAtIndex:row];
+    return cell;
+}
+
+#pragma mark -
+#pragma mark Table Delegate Methods
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSUInteger row = [indexPath row];
+    [self.list removeObjectAtIndex:row];
+    [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+}
 
 @end
